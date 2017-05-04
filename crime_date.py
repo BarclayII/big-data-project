a468c09f51f7d05a7607d1749420d06516c11dc7
Yuanjie Fu <fred.fu@nyu.edu>
2017-05-03 20:11:20 -0400

    pajipajipaji

--- crime_date.py
+++ crime_date.py
@@ -28,26 +28,11 @@ df = pd.DataFrame(np.matrix([dt,tp]).H, index=keys, columns=['date','type'])
 
 df['year'] = df['date'].map(lambda x: int(x.strftime("%Y")))
 df['day'] = df['date'].map(lambda x: x.strftime("%Y-%m-%d"))
+df = df.loc[df['year']>=2006]
 
-rawdate = df.groupby(['datestr'])[0].count()
-rawdate.plot()
-plt.show()
+countResult = df_clean.groupby(['day'])['type'].value_counts().to_frame().unstack(level=-1)
+countResult['total'] = df_clean.groupby(['day'])['type'].count()
 
-df_clean = df.loc[df['year']>=2006]
-df_clean['year'] = df_clean['date'].map(lambda x: int(x.strftime("%Y")))
-df_clean['hour'] = df_clean['date'].map(lambda x: x.strftime("%H"))
-
-cleandate = df_clean.groupby(['date'])['type'].count()
-cleandate.columns=['Crime_Num']
-cleantime = df_clean.groupby(['hour'])['type'].count()
-cleantime.columns=['Crime_Num']
-
-cleandate = pd.DataFrame(cleandate, index=pd.DatetimeIndex(cleandate.index))
-pv = pd.pivot_table(cleandate, index=cleandate.index.month, columns=cleandate.index.year, values='Crime_Num', aggfunc='mean')
-pv.plot()
-plt.show()
-
-cleantime.plot()
-plt.show()
+countResult.to_csv('count.csv')
 
 
