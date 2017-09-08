5f9ed774b6948c9b507b3b1206a1ab8935ed676c
Gan Quan <coin2028@hotmail.com>
2017-09-08 11:43:58 -0400

    google map polygon drawer

--- polygon.py
+++ polygon.py
@@ -22,3 +22,33 @@ def fix_polygon(poly_data, poly_key):
                         coords[1] = [coords[1]]
             poly[poly_key(feat['properties'])] = GEO.MultiPolygon(feat['geometry']['coordinates'])
     return poly
+
+
+def polydata(poly, color, stroke=None, fill=True, opacity=0.3, sw=1):
+    data = GEO.mapping(poly)
+
+    def json_code_point(lng, lat):
+        return 'new google.maps.LatLng(%f, %f)' % (lat, lng)
+
+    def json_code_pointset(points, name):
+        return '''var %s = [%s];''' % (
+                name,
+                ',\n'.join([json_code_point(lng, lat) for lng, lat in points])
+                )
+
+    if data['type'] == 'Polygon':
+        code = json_code_pointset(data['coordinates'][0], 'coords') + '''
+var polygon = new google.maps.Polygon({
+clickable: false,
+geodesic: true,
+fillColor: "%s",
+fillOpacity: %f,
+paths: coords,
+strokeColor: "%s",
+strokeOpacity: 1.000000,
+strokeWeight: %s
+});
+
+polygon.setMap(map);''' % (color, opacity, color if stroke is None else stroke, sw)
+
+    return code
